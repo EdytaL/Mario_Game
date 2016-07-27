a932683935d3299655f5754d06189717fde9abdc
EdytaL <lysiakedyta@gmail.com>
2016-07-27 23:54:14 +0200

    Modified event 'keydown', added function 'init()', added functions response for creating game's elements, added colision statements

--- js/app.js
+++ js/app.js
@@ -1,141 +1,151 @@
 document.addEventListener("DOMContentLoaded", function(){
     
-    //Variables - DOM searching
-    var myBorder = document.querySelector("body")
-    var myCanvas = document.querySelector("#canvas");
-    var ctx = myCanvas.getContext("2d");
-    var buttonStart = document.querySelector("#btn");
-    
-    //Variables game's elements (snake, coin, piont)
-    var snakeSize = 10;
-    var snakePositionX = 0;
-    var snakePositionY = 15;
-    
-    var coinSize = 10;
-    var points = 0;
-    var snake = new Array(2);
-    var coin;
-    var snakeLenght = 2;
+	//Variables - DOM searching
+	var canvas = document.querySelector("#canvas");
+	var ctx = canvas.getContext("2d");
     
     //Variables game field
-    var widthBoard = 350;
-    var heightBoard = 350;
-    var gameField = new Array(35);
-    var frameLength = 500; //new frame every 0.5 seconds
-    
-    //Loop - in order to build game matrix 
-    for(var i=0; i < gameField.length; i++) {
-        gameField[i] = new Array(35)
-    };
-    
-//buttonStart.addEventListener("click", function(event) { setInterval(function(){   
-    //Draw snake body 
-    function drawSnakeModule(){
+	var widthBoard = 350;
+	var heighBoard = 350;
+	
+	//Variables game's elements (snake, coin, point)
+	var cellSize = 10;
+	var direction;
+	var coin;
+	var score = 0;
+	
+	var snake; 
+	
+	function init(){
         
-        function drawSnakeBody(snakePositionX, snakePositionY) {
-        //This part draws single square of snake's body
-            ctx.fillStyle = "grey";
-            ctx.fillRect(snakePositionX*snakeSize, snakePositionY*snakeSize, snakeSize, snakeSize);
-            
-        //This part draws border for single square of snake's body
-            ctx.strokeStyle = "black";
-            ctx.strokeRect(snakePositionX*snakeSize, snakePositionY*snakeSize, snakeSize, snakeSize)
-            
-//            snakeMove();
-        };
+        //Initialize game
+		direction = "right"; //default direction
+		drawSnakeBody();
+		drawCoin(); 
+		
+		//Set interval for moving snake
+		if(typeof game_loop != "undefined") clearInterval(game_loop);
+		game_loop = setInterval(createElements, 80);
+	};
+	init();
+	
+	function drawSnakeBody(){
+        //Draws snake body 
+		var length = 3; //base lenght of snake
+		snake = []; 
         
-        function snakeMove() {
-              
-            drawSnakeBody(10,10);
+		for(var i = length-1; i>=0; i--)
+		{
+			snake.push({x: i, y:0});
+		}
+	};
+	
+	
+	function drawCoin()
+        //Generates random coins's position X and Y 
+	{
+		coin = {
+                x: Math.floor(Math.random() * (widthBoard/cellSize)) + 1,
+                y: Math.floor(Math.random() * (heighBoard/cellSize)) + 1};
             
-            snakePositionX += 1;
-            ctx.clearRect(0, 0, 350, 350); //clear the canvas
-            ctx.fillRect(snakePositionX,snakePositionY, 10, 10); //a moving snake
-            ctx.strokeRect(snakePositionX,snakePositionY, 10, 10); //a moving snake
-            setTimeout(snakeMove, frameLength); //do it all again
+	}
+	
+	
+	function createElements(){
+        
+		//Draw the game field
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, widthBoard, heighBoard);
+		ctx.strokeStyle = "black";
+		ctx.strokeRect(0, 0, widthBoard, heighBoard);
+		
+		var snakePositionX = snake[0].x;
+		var snakePositionY = snake[0].y;
+		
+        
+        //Statemenst -  move snake depend of keydown
+		if (direction == "right"){            snakePositionX++;
+        } else if (direction == "left"){      snakePositionX--;
+        } else if(direction== "up") {        snakePositionY--;
+        } else if(direction == "down"){      snakePositionY++;
         }
+		
+		//Collision statement - if snake is on the border of canvas - reset game
+		if(snakePositionX == -1 || snakePositionX == widthBoard/cellSize || snakePositionY == -1 || snakePositionY == heighBoard/cellSize || checkCollision(snakePositionX, snakePositionY, snake))
+		{
+			alert("Game over");
+            score = 0;
+            init();  
+            return;
+		}
+		
+		//Snake collect the coin
+		if(snakePositionX == coin.x && snakePositionY == coin.y)
+		{
+			var snakeTail = {x: snakePositionX, y: snakePositionY};
+			score++;
+			//Create new coin
+			drawCoin();
+		}
+		else
+		{
+			var snakeTail = snake.pop(); 
+			snakeTail.x = snakePositionX; snakeTail.y = snakePositionY;
+		}
+		
+		
+		snake.unshift(snakeTail); //puts back the snakeTail as the first cell
+		
+		for(var i = 0; i < snake.length; i++)
+		{
+			var cell = snake[i];
+			//Lets createElements 10px wide cells
+			createCell(cell.x, cell.y);
+		}
+		
+		
+		createCell(coin.x, coin.y);
+		//Lets createElements the score
+		var score_text = "Score: " + score;
+		ctx.fillText(score_text, 5, heighBoard-5);
+	}
+	
+	
+	function createCell(x, y){ 
+        //Draw elements of the body of snake
+		ctx.fillStyle = "blue";
+		ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
+		ctx.strokeStyle = "white";
+		ctx.strokeRect(x*cellSize, y*cellSize, cellSize, cellSize);
+	}
+	
+	function checkCollision(x, y, array){
+		
+		for(var i = 0; i < array.length; i++)
+		{
+			if(array[i].x == x && array[i].y == y)
+			 return true;
+		}
+		return false;
+	}
+	
+	//Lets add the keyboard controls now
+	document.addEventListener("keydown", function(event){
         
-        snakeMove()
-        
-         //Event activates when keyboard arrow is down
-        myBorder.addEventListener("keydown", function(event) {
-        
-            switch(event.keyCode) {
-            case 37: //Left
-                console.log("left");
-            break;
-            case 38: //Up
-                console.log("up");
-            break;
-            case 39: //Right
-                console.log("right");
-            break;
-            case 40: //Down
-                console.log("down");
-            break;
-            }
-        return gameField[snakePositionX][snakePositionY];
-        },false);
-        
-        
-        
-        function drawCoinToCollect () {
-            
-        //Generate random coins's position X and Y 
-            var randomPositionX = Math.floor(Math.random() * 35) + 1;
-            var randomPositionY = Math.floor(Math.random() * 35) + 1;
-            
-        //This part draws coin as single square
-            ctx.fillStyle = "yellow";
-            ctx.fillRect(randomPositionX*coinSize, randomPositionY*coinSize, coinSize, coinSize);
-        //This part draws coin's border
-            ctx.strokeStyle = "orange";
-            ctx.strokeRect(randomPositionX*coinSize, randomPositionY*coinSize, coinSize, coinSize);
-            
-        //Check the position of snake's body; coin can't be on the same position
-            
-            for(var i=0; i>snake.length; i++){
-                var snakeX = snake[i].x;
-                var snakeY = snake[i].y;
-                console.log(snakeX);
-            }
-            
-        }
-        
-        
-        function pointsCounter (){
-        //Count how many coins collected snake
-            var pointsText = "Score " + points;
-            ctx.fillStyle = "green";
-            ctx.fillText(pointsText, 145, heightBoard-10);
-        }
-//        drawSnakeBody(snakePositionX, snakePositionY);
-//        drawCoinToCollect();
-        pointsCounter();
-        
-        function enlargeSnakeBody() {
-            var snakeX = snake[0].x;
-            var snakeY = snake[0].y;
-            
-            if (direction === "right") {
-                snakeX++;
-            } else if (direction === "left") {
-                snakeX--;
-            } else if (direction === "up") {
-                snakeY--;
-            } else if (direction === "down") {
-                snakeY++;
-            };
-            
-            if (snakeX === -1 || snakeX === 36 || snakeY ==-1 || snakeY === 36) {
-                alert("Game over")
-            };
-        }
-
-    };
-    drawSnakeModule();
-    
-//}), 5000});
-    
-    
+        //By this event we can controll position of snake
+		var key = event.which;
+		
+		if(key == "37" && direction != "right") direction = "left";
+		else if(key == "38" && direction != "down") direction = "up";
+		else if(key == "39" && direction != "left") direction = "right";
+		else if(key == "40" && direction != "up") direction = "down";
+		
+	})
+	
+	
+	
+	
+	
+	
+	
 })
